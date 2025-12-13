9345e3a2c8ad48a4e444f47104a50efde7f2c615
apm-code <adrianignacio.perez832@comunidadunir.net>
2025-12-14 00:05:44 +0100

    Add: Bootstrap y estructura header/footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('gestion-productos');
 }
+
+//imports: [RouterOutlet]
