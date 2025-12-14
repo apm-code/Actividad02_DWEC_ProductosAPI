9257bbb16f4563e14b5aec9650c6459f4b2973a3
apm-code <adrianignacio.perez832@comunidadunir.net>
2025-12-14 01:56:52 +0100

    Add: ProductForm (Formulario para añadir productos)

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 import { ProductService, Product } from './services/product';
 import { ProductList } from './components/product-list/product-list';
+import { ProductsFormComponent } from './components/products-form/products-form';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductList],
+  imports: [ProductList, ProductsFormComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -18,6 +19,10 @@ export class App {
     this.productService.cargarProductos().subscribe(
       (datos : Product[]) => console.log('Productos cargados de la API: ',datos)
     )
+  }
+
+  onProductoCreado(producto: any){
+    console.log('Producto recibido: ', producto)
   }
 
 }
